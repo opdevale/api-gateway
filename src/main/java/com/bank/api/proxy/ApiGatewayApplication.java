6fd1725b3a5a70f6c24b0015e32815cf5016856a
Onkar Devale <opdevale@gmail.com>
2021-09-17 21:43:48 +0530

    comments

--- src/main/java/com/bank/api/proxy/ApiGatewayApplication.java
+++ src/main/java/com/bank/api/proxy/ApiGatewayApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Gateway Spring boot application
+ * @author Onkar Prakash Devale
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
